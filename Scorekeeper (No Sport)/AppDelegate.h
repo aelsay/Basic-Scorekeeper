eb51ae2e834d953a51911086d88997a667e76747
Ali ElSayed <aelsay28@gmail.com>
2013-09-26 16:56:36 -0400

    Initial Commit

---
+++ Scorekeeper (No Sport)/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Scorekeeper (No Sport)
+//
+//  Created by Ali ElSayed on 9/26/13.
+//  Copyright (c) 2013 Aperture Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Scorekeeper (No Sport)/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Scorekeeper (No Sport)
+//
+//  Created by Ali ElSayed on 9/26/13.
+//  Copyright (c) 2013 Aperture Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
